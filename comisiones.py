14ca711a6bad99f0302fa4cfc289cc66be88d342
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-18 19:40:22 -0600

    intento 1

--- comisiones.py
+++ comisiones.py
@@ -3,13 +3,25 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
-
+L = int(input("Ventas lunes: "))
+M = int(input("Ventas martes: "))
+X = int(input("Ventas miércoles: "))
+J = int(input("Ventas jueves: "))
+V = int(input("Ventas viernes: "))
 # Proceso
+ventas = (L+M+X+J+V)
 
+#comisión
+
+if ventas <= 20000:
+    comision = ventas * 0.05
+
+elif ventas > 20000:
+    comision = ventas * 0.06 
 
 # Salidas
-print(salida)
+print("Ventas totales: " + str(ventas))
+print ("Comisión: " + str(comision))
